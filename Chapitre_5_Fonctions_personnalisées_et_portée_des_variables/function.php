7a8017aa2a069e974dbaa7099d177445918104b1
mounir <mounirmaimouni21@gmail.com>
2026-02-02 08:22:18 +0000

    push

---
+++ Chapitre_5_Fonctions_personnalisées_et_portée_des_variables/function.php
@@ -0,0 +1,57 @@
+<?php
+
+
+// une fonction simple
+
+function names(){
+
+    $name = 'mounir maimouni';
+    $age = 20;
+    echo 'my name is' .' '. $name.' '. 'and I am ' .' '. $age .' '. 'years old . <br> ';
+}
+names();
+
+
+ 
+
+//Fonction avec argument et valeur de retour
+
+
+function numbres($num1,$num2){
+
+    $somme = $num1 + $num2;
+    return $somme;
+
+}
+echo 'somme :'.' '. numbres(10,20).'<br>';
+
+
+
+
+//Paramètre par défaut function
+
+function student($name='student'){
+
+    echo 'Hi'.' '. $name .'<br>';
+
+}
+student();
+
+
+
+
+
+//Tester la portée des variables
+
+function visiteurs() {
+    static $nb=0;
+    $nb++;
+    echo "Nombre de visiteurs : " . $nb . "<br>";
+}
+
+visiteurs() .'<br>'; 
+visiteurs() .'<br>'; 
+visiteurs() .'<br>'; 
+
+
+?>
